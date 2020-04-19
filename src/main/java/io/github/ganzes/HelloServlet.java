0d61f0e607c28a86b8a068f06fb7de6eba7af528
ganzes <charczuk.arkadiusz@gmail.com>
2020-04-19 21:24:50 +0200

    added 2 solutions to a challenge

--- src/main/java/io/github/ganzes/HelloServlet.java
+++ src/main/java/io/github/ganzes/HelloServlet.java
@@ -10,16 +10,30 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 
 @WebServlet(name = "Hello", urlPatterns = {"/api/*"})
 public class HelloServlet extends HttpServlet {
+    private static final String NAME_PARA = "name";
     private final Logger logger = LoggerFactory.getLogger(HelloServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         BasicConfigurator.configure();
 
-        logger.info("Request got");
-       resp.getWriter().write("Hello World!");
+        logger.info("Got request with parameters " + req.getParameterMap());
+
+        String name = Optional.ofNullable(req.getParameter(NAME_PARA)).orElse("World");
+
+        resp.getWriter().write("Hello " + name + "!");
+
+        /*String name = req.getParameter(NAME_PARA);
+
+        if (name != null){
+            resp.getWriter().write("Hello " + name + "!");
+        } else resp.getWriter().write("Hello World!");*/
+
+
+
     }
 }
